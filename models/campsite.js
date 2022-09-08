8255368df6dc47f1e5f3a2d974f35e4e96aff65d
kcksteve <60323908+kcksteve@users.noreply.github.com>
2022-09-07 21:28:54 -0400

    added mongoose populate for comments

--- models/campsite.js
+++ models/campsite.js
@@ -14,6 +14,10 @@ const commentSchema = new Schema({
     text: {
         type: String,
         required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 },
 {
--- models/user.js
+++ models/user.js
@@ -3,6 +3,14 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    fistname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
