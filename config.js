a66549e8c8772f98a39088a92808b9fa0a31515c
kcksteve <60323908+kcksteve@users.noreply.github.com>
2022-09-14 19:59:30 -0400

    Added facebook login auth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey': '54321-98765-23411-12345',
-    'mongoUrl': 'mongodb://localhost:27017'
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '472492391444450',
+        clientSecret: '53fee84363661c0d2168bd16fb90b323'
+    }
 }
